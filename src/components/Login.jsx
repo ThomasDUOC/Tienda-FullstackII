9f8f3bacb8e7a5a4a8173738bc1ecdd929f27e6b
Komotho <medina.itx@gmail.com>
2025-10-19 20:16:46 -0300

    Login (sin terminar)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { validateEmail, validatePassword }  from '../assets/js/validators.js';
+import { validateEmail, validatePassword } from '../assets/js/validators.js';
 
 function Login() {
 
@@ -13,7 +13,7 @@ function Login() {
     });
 
     const handleChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         if (name === 'email') {
             setEmail(value);
         } else if (name === 'password') {
@@ -22,15 +22,15 @@ function Login() {
     };
 
     const handleBlur = (e) => {
-        const {name} = e.target;
-        setTouched(prev => ({...prev, [name]: true}));
+        const { name } = e.target;
+        setTouched(prev => ({ ...prev, [name]: true }));
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (isEmailValid && isPasswordValid) {
-    // Aquí puedes agregar la lógica para enviar el formulario
-        window.location.href = 'index.html';
+            // lógica formualrio
+            window.location.href = 'index.html';
         }
     };
 
@@ -41,11 +41,11 @@ function Login() {
     return (
         <div>
             <div className="d-flex justify-content-center">
-                <img src={Logo} className='img-fluid' style={{width: '17.5%', height: 'auto'}} alt='Imagen'/>
+                <img src={Logo} className='img-fluid' style={{ width: '17.5%', height: 'auto' }} alt='Imagen' />
             </div>
 
             <div className="d-flex justify-content-center align-items-center vh-10">
-                <form onSubmit={handleSubmit} style={{color: "#ea39b8", minWidth: '500px', minHeight: '500px'}} noValidate>
+                <form onSubmit={handleSubmit} style={{ color: "#ea39b8", minWidth: '500px', minHeight: '500px' }} noValidate>
                     <fieldset>
                         <div className="d-flex justify-content-end">
                             <Link className='btn btn-primary' to='/'>X</Link>
@@ -54,7 +54,7 @@ function Login() {
 
                         <div className="mb-3">
                             <label htmlFor="emailInput" className="form-label">Correo</label>
-                            <input type="email" name="email" className={`form-control ${touched.email && (isEmailValid ? 'is-valid' : 'is-invalid')}`} id='emailInput' value={email} onChange={handleChange} onBlur={handleBlur} placeholder="Ingrese su correo" required/>
+                            <input type="email" name="email" className={`form-control ${touched.email && (isEmailValid ? 'is-valid' : 'is-invalid')}`} id='emailInput' value={email} onChange={handleChange} onBlur={handleBlur} placeholder="Ingrese su correo" required />
                             {touched.email && !isEmailValid && <div className="invalid-feedback d-block">Por favor, ingrese un correo electrónico válido.</div>}
                             {touched.email && isEmailValid && <div className="valid-feedback d-block">¡Correo electrónico válido!</div>}
                             <small className="form-text text-muted">No se compartiran tus datos.</small>
@@ -62,7 +62,7 @@ function Login() {
 
                         <div class="mb-3">
                             <label htmlFor="passwordInput" className="form-label">Contraseña</label>
-                            <input type="password" name="password" id="passwordInput" className={`form-control ${touched.password && (isPasswordValid ? 'is-valid' : 'is-invalid')}`}  placeholder="Ingrese su contraseña" value={password} onChange={handleChange} onBlur={handleBlur} required autoComplete='off'/>
+                            <input type="password" name="password" id="passwordInput" className={`form-control ${touched.password && (isPasswordValid ? 'is-valid' : 'is-invalid')}`} placeholder="Ingrese su contraseña" value={password} onChange={handleChange} onBlur={handleBlur} required autoComplete='off' />
                             {touched.password && !isPasswordValid && <div className="invalid-feedback d-block">La contraseña debe tener al menos 8 caracteres, incluir una
                                 mayúscula, un número y un carácter especial.</div>}
                             {touched.password && isPasswordValid && <div className="valid-feedback d-block">¡Contraseña válida!</div>}
