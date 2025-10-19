82d136113829640e70889c5274552f6099d8f5da
Komotho <medina.itx@gmail.com>
2025-10-19 20:15:56 -0300

    Cambiado navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import { Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import Home from './components/Home';
 import Login from './components/Login';
 import Contact from './components/Contact';
+import { Products } from './components/Products';
+import { ProductDetail } from './components/ProductDetail';
+import { Cart } from './components/Cart';
+import { Checkout } from './components/Checkout';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/navbar' element={<Navbar />} />
-      </Routes>
-      <Footer />
-    </>
+    <CartProvider>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/product/:id' element={<ProductDetail />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/navbar' element={<Navbar />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </CartProvider>
   );
 }
 
