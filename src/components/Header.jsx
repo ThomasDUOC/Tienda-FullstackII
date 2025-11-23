e624929a3d47337a837409307362590657e80479
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2025-11-23 11:50:07 -0300

    pequeños detalles de diseño en footer y header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
         <nav className='navbar navbar-expand-lg bg-primary' data-bs-theme='dark'>
             <div className='container-fluid'>
                 <img src={Logo} alt='Logo' style={{ width: '40px' }} />
-                <Link className='navbar-brand' to='/'>Level Up</Link>
+                <Link className='navbar-brand ms-3' to='/'>Level Up</Link>
                 <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarColor01' aria-controls='navbarColor01' aria-expanded='false' aria-label='Toggle navigation'>
                     <span className='navbar-toggler-icon'></span>
                 </button>
@@ -29,14 +29,14 @@ function Header() {
                             <Link className='nav-link' to='/contact'>Contacto</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className='nav-link' to='/login'>Login</Link>
+                            <Link className='nav-link' to='/login'>Iniciar Sesion</Link>
                         </li>
                     </ul>
-                    <form className="d-flex me-3">
+                    <form className="d-flex me-5">
                         <input className='form-control rounded-pill me-sm-2' type="search" placeholder='Buscar' />
                         <button className='btn btn-secondary rounded-4 my-2 my-sm-0' type='submit'>Buscar</button>
                     </form>
-                    <Link className='position-relative' to='/cart'>
+                    <Link className='position-relative me-3' to='/cart'>
                         <img src={Carrito} alt="Carrito" width={30} height={24} />
                         {cartCount > 0 && (
                             <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
