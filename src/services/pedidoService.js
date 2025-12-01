baba404fef319d1b0540de2be25fd64b1b890cd5
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2025-12-01 19:00:57 -0300

    se borran archivos innecesarios

--- src/services/pedidoService.js
+++
@@ -1,9 +0,0 @@
-import api from '../api/axiosConfig';
-
-export const pedidoService = {
-    createOrder: async (orderData) => {
-        // orderData debe coincidir con PedidoRequest de Java
-        const response = await api.post('/api/v1/pedidos', orderData);
-        return response.data;
-    }
-};
